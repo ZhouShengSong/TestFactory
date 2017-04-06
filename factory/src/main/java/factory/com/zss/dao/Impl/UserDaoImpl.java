8b9c73f17b4ca90d76f94ec847317162492c209b
Administrator <Administrator@bir3yk.net>
2017-04-06 16:33:47 +0800

    1.0

---
+++ factory/src/main/java/factory/com/zss/dao/Impl/UserDaoImpl.java
@@ -0,0 +1,13 @@
+package factory.com.zss.dao.Impl;
+
+import factory.com.zss.bean.UserBean;
+import factory.com.zss.dao.IuserDao;
+
+public class UserDaoImpl implements IuserDao {
+
+	public void say(UserBean user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ factory/src/main/java/factory/com/zss/dao/IuserDao.java
@@ -0,0 +1,8 @@
+package factory.com.zss.dao;
+
+import factory.com.zss.bean.UserBean;
+
+public interface IuserDao {
+
+	public void say(UserBean user);
+}
